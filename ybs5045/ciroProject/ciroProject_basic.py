8c21916957303e2cf274f675d3cda4aebf806625
Mustafa Volkan Gungor <mvolkang@gmail.com>
2018-10-20 19:53:09 +0300

    basic and integer entry check programs added

---
+++ ybs5045/ciroProject/ciroProject_basic.py
@@ -0,0 +1,16 @@
+#toplam ciro hesaplama
+def ciro(sm,bsf):
+  return sm*bsf
+
+#adam basi ciro hesaplama
+def adamBasiCiro(tCiro,tCalisan):
+  return tCiro/tCalisan
+
+#Çalisan sayisi girdisi:
+tCalisan=float(input("Çalisan sayisi giriniz: "))
+
+#Satis Miktari ve Birim Satis Fiyati:
+sm=float(input("Satis Miktari giriniz: "))
+bsf=float(input("Birim Satis Fiyati giriniz: "))
+
+print(f"Adam basi ciro: {adamBasiCiro(ciro(sm,bsf),tCalisan):0.2f} TL")
