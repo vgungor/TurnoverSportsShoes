8f018f980344dea4806fbff137faad02459216af
Mustafa Volkan Gungor <mvolkang@gmail.com>
2018-10-21 11:50:02 +0300

    toplam çalışan global değişken 25 atandı

--- ybs5045/ciroProject/ciroProject_basic.py
+++ ybs5045/ciroProject/ciroProject_basic.py
@@ -1,16 +1,24 @@
+#global variable total çalışan
+global tCalisan
+tCalisan = 25
+
 #toplam ciro hesaplama
 def ciro(sm,bsf):
-  return sm*bsf
+    return sm*bsf
 
 #adam basi ciro hesaplama
 def adamBasiCiro(tCiro,tCalisan):
-  return tCiro/tCalisan
+    return tCiro/tCalisan
 
-#Çalisan sayisi girdisi:
-tCalisan=float(input("Çalisan sayisi giriniz: "))
+def main():
+    
+    #Çalisan sayisi girdisi:
+    #tCalisan=float(input("Çalisan sayisi giriniz: "))
 
-#Satis Miktari ve Birim Satis Fiyati:
-sm=float(input("Satis Miktari giriniz: "))
-bsf=float(input("Birim Satis Fiyati giriniz: "))
+    #Satis Miktari ve Birim Satis Fiyati:
+    sm=float(input("Satis Miktari giriniz: "))
+    bsf=float(input("Birim Satis Fiyati giriniz: "))
 
-print(f"Adam basi ciro: {adamBasiCiro(ciro(sm,bsf),tCalisan):0.2f} TL")
+    print(f"Adam basi ciro: {adamBasiCiro(ciro(sm,bsf),tCalisan):0.2f} TL")
+
+main()
